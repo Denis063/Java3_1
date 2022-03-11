08e510782de97ca1cedfb84c179da9f27bd3cc88
Денис Сергеев <D-D1983@yandex.ru>
2022-03-11 11:56:05 +0400

    update files

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+    public int calculate(
+            int cost
+    ) {
+        // TODO: Принимает на вход один параметр: cost типа int
+        // TODO: Возвращает рассчитанное количество миль (тип - int)
+        return cost / 20;
+    }
+}
